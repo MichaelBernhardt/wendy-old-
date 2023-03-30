11fef0d161c9c397189716080c4066c637b993e9
MichaelBernhardt <earthlingmikey@gmail.com>
2023-03-30 18:27:21 +0200

    Added style to app.js

--- src/App.js
+++ src/App.js
@@ -63,4 +63,58 @@ const App = () => {
     <View style={styles.container}>
       <ScrollView>
         {messages.map((msg, index) => (
-          <Text key={index
+          <Text key={index} style={msg.sender === 'user' ? styles.userMessage : styles.gptMessage}>
+            {msg.text}
+          </Text>
+        ))}
+      </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={message}
+          onChangeText={setMessage}
+          style={styles.input}
+          placeholder="Type your message"
+        />
+        <Button title="Send" onPress={handleSendMessage} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    paddingLeft: 5,
+  },
+  userMessage: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#c3c3c3',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+  },
+  gptMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#007AFF',
+    color: 'white',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+  },
+});
+
+export default App;
